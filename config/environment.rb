6f0155e19324658438bc6d397946fb8782dffca0
Section9-Togusa <corbindac@gmail.com>
2020-02-10 05:25:42 -0500

    Done.

--- config/environment.rb
+++ config/environment.rb
@@ -4,3 +4,10 @@ Bundler.require
 require_relative '../lib/student'
 
 DB = {:conn => SQLite3::Database.new("db/students.db")}
+
+# Student.create_table
+# josh = Student.new("Josh", "1")
+# daryl = Student.new("Daryl", "8")
+ 
+# josh.save
+# daryl.save
