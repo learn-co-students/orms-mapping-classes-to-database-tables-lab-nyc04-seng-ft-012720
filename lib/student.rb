71defe0e23722c4e25e8a2fb83106cdc0db110fa
Section9-Togusa <corbindac@gmail.com>
2020-02-10 09:05:47 -0500

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -1,45 +1,44 @@
-class Student
+class Student 
+  attr_accessor :name, :grade 
+  attr_reader :id 
 
-  attr_accessor :name, :grade
-  attr_reader :id
-
-  def initialize(name, grade, id = nil) 
+  def initialize(name, grade, id = nil)
     @id = id 
-    @name = name
+    @name = name 
     @grade = grade 
-    # ALL << self 
   end 
 
-  def self.create_table
-    sql = <<-SQL
+  def self.create_table 
+    sql = <<-SQL 
     CREATE TABLE IF NOT EXISTS students (id INTEGER PRIMARY KEY, name TEXT, grade INTEGER)
-      SQL
+    SQL
     DB[:conn].execute(sql)
   end 
 
   def self.drop_table 
     sql = <<-SQL
-      DROP TABLE students 
-      SQL
-    
+    DROP TABLE students 
+    SQL
     DB[:conn].execute(sql)
-  end 
-  
-  def save 
-    sql = <<-SQL
+  end
 
-    INSERT INTO students (name, grade) VALUES (?,?)
+  def save
+    sql = <<-SQL
+    INSERT INTO students (name,grade) VALUES (?,?)
     SQL
     DB[:conn].execute(sql, self.name, self.grade)
 
-    @id = DB[:conn].execute("SELECT last_insert_rowid() FROM students")[0][0]
+    @id = DB[:conn].execute("SELECT last_insert_rowid() FROM students ")[0][0]
   end 
 
-  def self.create(name:, grade:)
-    student = Student.new(name, grade)
+  def self.create(name:,grade:)
+    student = Student.new(name,grade)
     student.save
     student
-  end
-  
+  end 
 
-end
+
+
+
+
+end 
